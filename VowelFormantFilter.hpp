4d8a9be961d20e14e55c3519cfda0149b8d3c824
Chip Audette <chipaudette@yahoo.com>
2021-05-30 12:24:30 -0400

    VowelFormant: add 2nd model

--- VowelFormantFilter.hpp
+++ VowelFormantFilter.hpp
@@ -81,11 +81,12 @@ class VowelFormantFilter : public SampleBasedPatch {
 		VowelFormantFilter(void) {
 			registerParameter(PARAMETER_A, "Vowel"); //will be 0.0 to 1.0
 			registerParameter(PARAMETER_B, "Q"); //will be 0.0 to 1.0
-			//registerParameter(PARAMETER_C, "Fc3"); //will be 0.0 to 1.0
+			registerParameter(PARAMETER_C, "Model"); //will be 0.0 to 1.0
 			registerParameter(PARAMETER_D, "gain");
 
 			//initialize states
 			overall_gain = 1.0;
+			model = -1;
 			for (int i=0; i<3; i++) {
 				low[i] = 0.0;
 				band[i]=0.0;
@@ -97,8 +98,12 @@ class VowelFormantFilter : public SampleBasedPatch {
 			float fc[3];
 			float vowel = getParameterValue(PARAMETER_A); //a value of 1.0 means fc = sample rate
 			q = getParameterValue(PARAMETER_B); //a value of 1.0 means fc = sample rate
-			//fc[2] = getParameterValue(PARAMETER_C); //a value of 1.0 means fc = sample rate
+			float new_model = getParameterValue(PARAMETER_C);  
 			overall_gain = getParameterValue(PARAMETER_D);
+			
+			//update the formant model
+			chooseModel(new_model);
+			
 
 			// fc = cutoff freq in Hz 
 			// fs = sampling frequency //(e.g. 44100Hz)
@@ -128,6 +133,38 @@ class VowelFormantFilter : public SampleBasedPatch {
 			overall_gain = overall_gain * 2.0;  //make the center of the dial be zero gain.  max will be G=2 => 6dB
 			overall_gain = overall_gain * overall_gain;  //max gain will be 4 => 12 dB
 		}
+		
+		//choose which formant model to use
+		int chooseModel(int new_model) {
+			
+			//decide if we reset the model or not
+			if ( (model < 0) ||
+				 ((model == 1) && (new_model > 0.52)) ||
+				 ((model == 2) && (new_model < 0.48)) ) {
+					 
+				//yes, do reset the model
+				model = 1;
+				if (new_model > 0.5) model = 2;
+				
+				switch (model) {
+					case 1:
+						N_bandpass = N_bandpass_1;
+						N_table = N_table_1;
+						table_F1 = table_F1_1; table_F2 = table_F2_1; table F3 = table_F3_1;
+						table_gain_F1 = table_gain_F1_1; table_F2 = table_gain_F2_1; table F3 = table_gain_F3_1;
+						break;
+						
+					case 2:
+						N_bandpass = N_bandpass_2;
+						N_table = N_table_2;
+						table_F1 = table_F1_2; table_F2 = table_F2_2; table F3 = table_F3_2;
+						table_gain_F1 = table_gain_F1_2; table_F2 = table_gain_F2_2; table F3 = table_gain_F3_2;
+						break;
+				}
+			}
+			return model;
+		}
+		
 		float bandpass(float sample, int ind) {
 		
 			low[ind] = low[ind] + f[ind] * band[ind];
@@ -144,24 +181,38 @@ class VowelFormantFilter : public SampleBasedPatch {
 		}
   
   private:
-	  float low[3], band[3];
-	  float f[3], q;
-	  float gain[3];
-	  float overall_gain;
-	  
-	  #define FORMANT_MODEL 1
-	  #if (FORMANT_MODEL == 1)
-		  //Canadian english vowels https://home.cc.umanitoba.ca/~krussll/phonetics/acoustic/formants.html
-		  const int N_bandpass = 2; //how many bandpass filters to use
-		  const int N_table = 11; //how long are the arrays below
-		  float table_F1[11] = {280.,	370.,	405.,	600.,	860.,	830.,	560.,	430.,	400.,	330.,	680. };
-		  float table_F2[11] = {2230.,	2090.,	2080.,	1930.,	1550.,	1170.,	820.,	980.,	1100.,	1260.,	1310. };
-		  float table_F3[11] = {0., 	0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,};
-		  float table_gain_F1[11] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};	  
-		  float table_gain_F2[11] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};
-		  float table_gain_F3[11] = {0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0};
-	 #endif
+		float low[3], band[3];
+		float f[3], q;
+		float gain[3];
+		float overall_gain;
+		int model;
 
+		  
+		#define MAX_TABLE 16
+		int N_bandpass, N_table;
+		float *table_F1, *table_F2, *table_F3;
+		float *table_gain_F1, *table_gain_F2, *table_gain_F2;
+
+		//Canadian english vowels https://home.cc.umanitoba.ca/~krussll/phonetics/acoustic/formants.html
+		const int N_bandpass_1 = 2; //how many bandpass filters to use
+		const int N_table_1 = 11; //how long are the arrays below
+		float table_F1_1[MAX_TABLE] = {280.,	370.,	405.,	600.,	860.,	830.,	560.,	430.,	400.,	330.,	680. };
+		float table_F2_1[MAX_TABLE] = {2230.,	2090.,	2080.,	1930.,	1550.,	1170.,	820.,	980.,	1100.,	1260.,	1310. };
+		float table_F3_1[MAX_TABLE] = {0., 	0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,};
+		float table_gain_F1_1[MAX_TABLE] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};	  
+		float table_gain_F2_1[MAX_TABLE] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};
+		float table_gain_F3_1[MAX_TABLE] = {0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0};
+
+		//wikipedia: https://en.wikipedia.org/wiki/Formant
+		const int N_bandpass_2 = 2; //how many bandpass filters to use
+		const int N_table_2 = 16-3-3; //how long are the arrays below
+		float table_F1_2[MAX_TABLE] = {240., 390., 610., 850., 820., 750., 700., 500., 360., 250.};
+		float table_F2_2[MAX_TABLE] = {2400., 2300., 1900., 1610., 1530., 940., 760., 700., 640., 595.};
+		float table_F3_2[MAX_TABLE] = {0., 	0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,		0.,};
+		float table_gain_F1_2[MAX_TABLE] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};	  
+		float table_gain_F2_2[MAX_TABLE] = {1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0};
+		float table_gain_F3_2[MAX_TABLE] = {0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0};		 
+	
 };
 
 #endif /* __StateVariableFilterPatch_hpp__ */
@@ -179,20 +230,20 @@ F1 – F2
 (Hz)
 IPA	F1	F2		Diff
 i	240	2400	2160
-y	235	2100	1865
-e	390	2300	1910
-ø	370	1900	1530
+y	235	2100	1865 //don't use
+e	390	2300	1910 
+ø	370	1900	1530 //don't use
 ɛ	610	1900	1290
-œ	585	1710	1125
+œ	585	1710	1125  //don't use
 a	850	1610	760
 ɶ	820	1530	710
 ɑ	750	940		190
 ɒ	700	760		60
-ʌ	600	1170	570
+ʌ	600	1170	570   //don't use
 ɔ	500	700		200
-ɤ	460	1310	850
+ɤ	460	1310	850  //don't use
 o	360	640		280
-ɯ	300	1390	1090
+ɯ	300	1390	1090  //don't use
 u	250	595		345
 
 https://home.cc.umanitoba.ca/~krussll/phonetics/acoustic/formants.html
